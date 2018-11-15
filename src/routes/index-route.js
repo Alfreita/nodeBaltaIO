4813b33c567b206c1150d093b125896514f676c9
Victor Almeida <victoralmeida@ibm.com>
2018-11-15 16:13:23 -0200

    my first commit

---
+++ src/routes/index-route.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).send({
+        title:"Node api",
+        version:"0.0.1"
+    });
+});
+
+router.get('/index',(req,res,next)=>{
+    res.render("index.ejs");
+});
+
+
+router.get('/produtos',(req,res,next)=>{
+    res.render("form.ejs");
+});
+module.exports = router;
